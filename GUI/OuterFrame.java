769bdb703e2846ff5ec2e9d9f53817c8e5e0ebfa
Kai Sandstrom <ksandstrom2@gmail.com>
2022-07-14 11:13:09 -0400

    Removed unnecessary variables in OuterFrame.java

--- GUI/OuterFrame.java
+++ GUI/OuterFrame.java
@@ -14,15 +14,15 @@ public class OuterFrame {
     }
 
     public void initPanel(int difficulty) {
-        int rows, cols, numMines;
+        int rows, cols;
         switch (difficulty) {
             case Game.BEGINNER:
-                rows = 9; cols = 9; numMines = 10; break;
+                rows = 9; cols = 9; break;
             case Game.EXPERT:
-                rows = 16; cols = 30; numMines = 99; break;
+                rows = 16; cols = 30; break;
             case Game.INTERMEDIATE:
             default:
-                rows = 16; cols = 16; numMines = 40;
+                rows = 16; cols = 16;
         }
         frame.setTitle("jMinesweeper by Kai Sandstrom");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
